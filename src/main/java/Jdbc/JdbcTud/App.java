07c6d637541ab5de49dd9cb42488c5af3fb05f3f
jpodlesny <jpodlesny@sigma.ug.edu.pl>
2016-11-16 16:15:36 +0100

    szkielet projektu

---
+++ src/main/java/Jdbc/JdbcTud/App.java
@@ -0,0 +1,13 @@
+package Jdbc.JdbcTud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
